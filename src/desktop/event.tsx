394617ef39356ac6f3e7d7f3b8993cc1ae9e1a41
Ribbit <echo07200503@gmail.com>
2023-11-27 14:11:21 +0900

    ♻ Refactor event.tsx to add root element for theme plugin

--- src/desktop/event.tsx
+++ src/desktop/event.tsx
@@ -5,28 +5,33 @@ import { getAppId, getHeaderSpace } from '@lb-ribbit/kintone-xapp';
 
 import App from './app';
 
-const WRAPPER_CLASS = 'local-bias_theme_area-wrapper';
+const ROOT_ID = 'ribbit-kintone-plugin-theme-root';
 
 const events: launcher.Events = ['app.record.index.show'];
 
 const action: launcher.Action = async (event, pluginId) => {
-  let wrapper = document.querySelector(`.${WRAPPER_CLASS}`);
-
-  if (!wrapper) {
-    wrapper = document.createElement('div');
-    wrapper.classList.add(WRAPPER_CLASS);
-
-    const headerSpace = getHeaderSpace(event.type);
-    if (headerSpace) {
-      headerSpace.append(wrapper);
-    }
+  if (document.getElementById(ROOT_ID)) {
+    return event;
   }
+
+  const headerSpace = getHeaderSpace(event.type);
+  if (!headerSpace) {
+    console.warn('No header space found');
+    return event;
+  }
+
+  const rootElement = document.createElement('span');
+  rootElement.id = ROOT_ID;
+  rootElement.style.display = 'inline-flex';
+  rootElement.style.padding = '0 8px';
+
+  const root = createRoot(rootElement);
 
   const response = await kintoneClient.app.getAppSettings({ app: getAppId()! });
 
   console.log(response);
 
-  createRoot(wrapper).render(<App initSettings={response} />);
+  root.render(<App initSettings={response} />);
 
   return event;
 };
